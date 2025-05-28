a8216c74e0c8861df09ad3a25242b2e95f2a24e5
Imsquae <145940078+Imsquae@users.noreply.github.com>
2025-05-28 15:42:03 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,15 @@
-import sys
 import os
+import sys
 
-# Chemin absolu vers le répertoire du projet sur PythonAnywhere
-project_home = '/var/www/squae_pythonanywhere_com'
+# Ajouter le répertoire du projet au PYTHONPATH
+project_home = os.path.dirname(os.path.abspath(__file__))
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Ajouter le répertoire parent au PYTHONPATH
-parent_dir = os.path.dirname(project_home)
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
-
-# import flask app but need to call it "application" for WSGI to work
+# Importer l'application Flask
 from app import create_app
 
+# Créer l'application
 application = create_app()
 
 # Si vous utilisez des variables d'environnement, assurez-vous qu'elles sont chargées
